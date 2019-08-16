bea5b2ca4bda2951ae3dc891fb0cee6b6a3fbea4
Dejaku <54068284+Dejaku@users.noreply.github.com>
2019-08-16 19:07:47 -0400

    adding files for the first time

---
+++ massages.js
@@ -0,0 +1,14 @@
+var massagesPerWeek = prompt("how many massages per week? (estimate hours worked per week)");
+//Test that is is connected in working by using alert(massagesPerWeek)
+
+var massagesPerMonth = massagesPerWeek * 3.5;
+
+var massagesPerYear= massagesPerMonth * 12
+
+var yearsMassaging = prompt("How many years have you been massaging?")
+
+var massagesTotal = massagesPerMonth * yearsMassaging
+
+alert("You have done roughly " + massagesTotal + " massages in " + yearsMassaging + " years");
+
+console.log("You have done roughly " + massagesTotal + " massages in " + yearsMassaging + " years");
